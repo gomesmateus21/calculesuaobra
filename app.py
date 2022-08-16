c516ef0cb8fc09dce291ce7aa12d108628fdf924
gomesmateus21 <gomesmateus21@gmail.com>
2022-08-16 18:23:50 -0300

    adicionando lista python

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    calculos = ["pisos", "tijolos", "contrapiso", "tinta"]
+    return render_template("index.html", calculos=calculos)
 
 
 if __name__ == "__main__":
