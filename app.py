11c07bddfbc71fb56dc962eb28db5b3e4693a699
gomesmateus21 <gomesmateus21@gmail.com>
2022-08-18 13:10:03 -0300

    Atualização do layout

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    calculos = ["pisos", "tijolos", "contrapiso", "tinta"]
+    calculos = ["Pisos", "Tijolos", "Contrapiso", "Tinta"]
     return render_template("index.html", calculos=calculos)
 
 
