487ead6fd448c05e3d25347109ad1a00d997dde4
gomesmateus21 <gomesmateus21@gmail.com>
2022-08-22 17:00:02 -0300

    fix mobile

---
+++ static/script.js
@@ -0,0 +1,14 @@
+const meuCabecalho = document.querySelector('h1')
+    meuCabecalho.textContent = 'Ola Mundo '
+
+
+const btnMobile = document.getElementById('btn-mobile');
+
+    function toggleMenu(){
+        const nav = document.getElementById('nav')
+        nav.classList.toggle('active')
+    }
+
+    btnMobile.addEventListener('click', toggleMenu);
+
+    
